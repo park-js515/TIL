63b273a3422840c0e692d2240d0987ba06c8a86c
박주성 <hdcom37@gmail.com>
2023-01-16 17:56:29 +0900

    5th

--- code/bubble.py
+++ code/bubble.py
@@ -17,3 +17,33 @@ def bubble_seq(a):
 # print(bubble_seq([1000, -1, 123, 25, 30, -30, 5]))
 
 # print(bubble_seq(["hello", "hi", "python", "world"]))
+
+## 스트링의 아스키 코드값 반환받기
+def str_score(s):
+    a = len(s)
+    lst = [0]*a
+
+    for i in range(a):
+        lst[i] = ord(s[i])
+    
+    return lst
+
+## 정렬 (불완전?)
+def str_compare(s1, s2):
+    s11 = s1 + " "
+    s21 = s2 + " "
+
+    k = 0
+    ret = [s1, s2]
+    while(True):
+        if (s11 > s21):
+            ret = [s2, s1]
+            break;
+        if (s11 < s21):
+            break;
+        if ((k + 1) == len(s11) or (k + 1) == len(s21)):
+            break;
+    
+    return(ret)
+
+str_compare("hello", "asdf")
--- code/go.py
+++ code/go.py
@@ -1,23 +1,6 @@
-# 이름받고 hello, hi, python, world 사전식 정렬 sorted 없이 해보기 17차시
-def str_score(s):
-    a = len(s)
-    lst = []*a
+s = 0
+for i in range(1, 1001):
+    if (i % 2 == 0 or i % 7 == 0):
+        s += i
 
-    for i in range(a):
-        lst[i] = ord(s[i])
-    
-    return lst
-
-def bubble_seq(a):
-    lst = a
-    length = len(a) - 1
-    k = len(a)
-    
-    for i in range(length):
-        k -= 1
-        for j in range(0, k):
-            if (lst[j] > lst[j + 1]):
-                temp = lst[j]
-                lst[j] = lst[j + 1]
-                lst[j + 1] = temp
-    return (lst)
+print(s)
