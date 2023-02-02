6d63907a862b2b63f9e6998bcc3b6fe212c25aed
박주성 <hdcom37@gmail.com>
2023-02-02 17:55:26 +0900

    ver1.053

--- study/test2.py
+++ study/test2.py
@@ -4,32 +4,55 @@ sys.stdin = open("input.txt", "r")
 # import sys
 # input = sys.stdin.readline
 
-def clr(a, b):
-    if (a == 0 or b == 0):
-        return a + b
-    if (a == b):
-        return a
+# 2578
+def bingo(lst):
+    cnt = 0
+
+    temp_cnt = 0
+    for i in range(5):
+        if lst[i][i] == 0:
+            temp_cnt += 1
     
-    return a + b
+    if temp_cnt == 5:
+        cnt += 1
 
-T = int(input())    
+    temp_cnt = 0
+    for i in range(5):
+        if lst[i][4 - i] == 0:
+            temp_cnt += 1
 
-for test_case in range(1, T + 1):
-    n = int(input())
-    total = 0
+    if temp_cnt == 5:
+        cnt += 1
 
-    for i in range(n):
-        a, b, c, d, e = map(int, input().split())
-        field = [[0]*10 for i in range(10)]
+    for i in range(5):
+        if lst[i] == [0]*5:
+            cnt += 1
 
-        for j in range(a, c + 1):
-            for k in range(b, d + 1):
-                field[j][k] = clr(field[j][k], e)
+    for i in range(5):
+        temp_cnt = 0
+        for j in range(5):
+            if lst[j][i] == 0:
+                temp_cnt += 1
+        
+        if temp_cnt == 5:
+            cnt + 1
 
-    for j in range(10):
-        for k in range(10):
-            if (field[j][k] == 3):
-                total += 1
+    return cnt
 
-    # print(f"#{test_case} {total}")
-    print(field)
+
+lst = [list(map(int, input().split())) for i in range(5)]
+go = [list(map(int, input().split())) for i in range(5)]
+go = sum(go, [])
+
+n = 0
+num = go[n]
+
+while (bingo(lst) < 3):
+    for i in range(5):
+        for j in range(5):
+            if lst[i][j] == num:
+                lst[i][j] = 0
+    
+    n += 1
+
+print(1)
