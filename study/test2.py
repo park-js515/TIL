436207ac7e38e4e85f8bef6aead9bd119af9d889
박주성 <hdcom37@gmail.com>
2023-02-03 17:54:52 +0900

    ver1.055

--- study/test2.py
+++ study/test2.py
@@ -4,94 +4,55 @@ sys.stdin = open("input.txt", "r")
 # import sys
 # input = sys.stdin.readline
 
-# 2578
-# 2477 참외밭
-
-def ftn(start, a, b): # 동서남북
-    x = [
-        [0, 1],
-        [0, -1],
-        [1, 0],
-        [-1, 0]
-    ]
-
-    temp = x[a - 1]
-    move = [temp[0] * b, temp[1] * b]
-    ret = [start[0] + move[0], start[1] + move[1]]
-
-    return ret
+# 2527 직사각형
+def absol(n):
+    if n < 0:
+        return -n
+    else:
+        return n
 
 
-def ftn2(lst):
-    row = []
-    col = []
+def far(lst):
+    a = lst[:2]
+    b = lst[2:4]
+    c = lst[4:6]
+    d = lst[6:]
 
-    for i in lst:
-        a = i[0]
-        b = i[1]
+    ma = 0
+    p1 = None
+    p2 = None
+    for i in [a, b]:
+        for j in [c, d]:
+            temp = absol(i[0] - j[0]) + absol(i[1] - j[1])  # manhatan distance
+            if temp > ma:
+                ma = temp
+                p1, p2 = i, j
 
-        if a not in row:
-            row.append(a)
-        if b not in col:
-            col.append(b)
-
-    return (row, col)
-
-
-def ftn3(m):
-    for i in range(2):
-        k1 = m[i]
-        for j in range(1, 3):
-            if k1[0] != m[j][0]:
-                if k1[1] != m[j][1]:
-                    k2 = m[j]
-                    return k1, k2
+    return (p1, p2)
 
 
-def bubble(lst):
-    temp = lst[:]
-    lg = len(temp)
+# def lin(lst):
+#     a = lst[:2]
+#     b = lst[2:4]
+#     c = lst[4:6]
+#     d = lst[6:]
 
-    for i in range(lg - 1):
-        for j in range(lg - 1 - i):
-            if temp[j] > temp[j + 1]:
-                temp[j], temp[j + 1] = temp[j + 1], temp[j]
+#     l11 = absol(a[0] - b[0])
+#     l12 = absol(a[1] - b[1])
+#     l21 = absol(c[0] - d[0])
+#     l22 = absol(c[1] - d[1])
 
-    return temp
-
-##################################################################
-
-val = int(input())
-start = [0, 0]
-point = []
-
-for i in range(6):
-    a, b = map(int, input().split())
-    temp = ftn(start, a, b)
-    point.append(temp)
-    start = temp
-
-rows, cols = ftn2(point)
-rows = bubble(rows)
-cols = bubble(cols)
-mr = rows[1]
-mc = cols[1]
-
-m = []
-
-for i in point:
-        if i[0] == mr and i not in m:
-            m.append(i)
-        
-        if i[1] == mc and i not in m:
-            m.append(i)
+#     return l11, l12, l21, l22
 
 
-k1, k2 = ftn3(m)
+###################################################################
+for i in range(4):
+    lst = list(map(int, input().split()))
+    p1, p2 = far(lst)
+
+    print(f"#{i}: {p1}, {p2}")
 
 
-total = (rows[2] - rows[0]) * (cols[2] - cols[0])
-fraction = (k1[0] - k2[0]) * (k1[1] - k2[1]) if ((k1[0] - k2[0]) * (k1[1] - k2[1]) > 0) else -((k1[0] - k2[0]) * (k1[1] - k2[1]))
-ret = (total - fraction) * val
 
-print(ret)
+    # print(f"#{i}: {p1}, {p2}")
+    # print(f"#{i}: {p1w}, {p1h}, {p2w}, {p2h}")
