bb16c4b2de5b24976f32a842a67267a2efd225ae
Juseong <hdcom37@gmail.com>
2023-02-04 16:32:43 +0900

    ver1.056

--- study/test2.py
+++ study/test2.py
@@ -4,55 +4,27 @@ sys.stdin = open("input.txt", "r")
 # import sys
 # input = sys.stdin.readline
 
-# 2527 직사각형
-def absol(n):
-    if n < 0:
-        return -n
+# 10158 개미, 원큐에 도착해야 한다.
+# 가로와 세로를 나누어서 생각한다.
+# 자신의 축의 두 배에 해당하는 값이 넣어지면 제자리로 돌아온다. (좌우로 왔다갔다 했다는 것이다.)
+
+def move(x, y, z): # 현재 위치, 이동, 축의 범위
+    if x + y <= z:
+        return x + y
     else:
-        return n
+        temp = 2*z - x - y
+        if  temp > 0:
+            return temp
+        else:
+            return - temp
 
+w, h = map(int, input().split()) # 가로 세로
+p, q = map(int, input().split()) # 축의 값
+t = int(input())
+t1 = t % (2 * w)
+t2 = t % (2 * h)
+    
+res1 = move(p, t1, w)
+res2 = move(q, t2, h)
 
-def far(lst):
-    a = lst[:2]
-    b = lst[2:4]
-    c = lst[4:6]
-    d = lst[6:]
-
-    ma = 0
-    p1 = None
-    p2 = None
-    for i in [a, b]:
-        for j in [c, d]:
-            temp = absol(i[0] - j[0]) + absol(i[1] - j[1])  # manhatan distance
-            if temp > ma:
-                ma = temp
-                p1, p2 = i, j
-
-    return (p1, p2)
-
-
-# def lin(lst):
-#     a = lst[:2]
-#     b = lst[2:4]
-#     c = lst[4:6]
-#     d = lst[6:]
-
-#     l11 = absol(a[0] - b[0])
-#     l12 = absol(a[1] - b[1])
-#     l21 = absol(c[0] - d[0])
-#     l22 = absol(c[1] - d[1])
-
-#     return l11, l12, l21, l22
-
-
-###################################################################
-for i in range(4):
-    lst = list(map(int, input().split()))
-    p1, p2 = far(lst)
-
-    print(f"#{i}: {p1}, {p2}")
-
-
-
-    # print(f"#{i}: {p1}, {p2}")
-    # print(f"#{i}: {p1w}, {p1h}, {p2w}, {p2h}")
+print(res1, res2)
