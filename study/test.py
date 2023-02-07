21de7765c9af62a21e9caced69aeb8fb498ef910
Juseong <hdcom37@gmail.com>
2023-02-07 22:39:50 +0900

    ver1.061

--- study/test.py
+++ study/test.py
@@ -4,57 +4,33 @@ sys.stdin = open("input.txt", "r")
 # import sys
 # input = sys.stdin.readline
 
-def chg(ch):
-    chg1 = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+n = int(input())
+res = []
+lst = []
+k = 1
+tf = True
 
-    return chg1[ch]
+for i in range(n):
+    now = int(input())
 
-def compare(cnt, restrict):
-    ret = [0]*4
-    
-    for i in range(4):
-        if cnt[i] >= restrict[i]:
-            ret[i] += 1
+    if k <= now:
+        while k <= now:
+            lst.append(k)
+            res.append('+')
+            k += 1
+ 
+        lst.pop()
+        res.append('-')
 
-    return ret
+    elif k > now:
+        a = lst.pop()
+        res.append('-')
+        if a != now:
+            tf = False
+            break
 
-def step(ch, i):
-    global st, cnt, restrict, check
-
-    # minus
-    a = st[i - 1]
-    idx1 = chg(a)
-    cnt[idx1] -= 1
-
-    if cnt[idx1] < restrict[idx1] and check[idx1] == 1:
-        check[idx1] -= 1
-
-    # plus
-    idx2 = chg(ch)
-    cnt[idx2] += 1
-
-    if cnt[idx2] >= restrict[idx2] and check[idx2] == 0:
-        check[idx2] += 1
-
-
-s, p = map(int, input().split())
-st = list(input())
-restrict = list(map(int, input().split())) # A C G T
-cnt = [0] * 4
-res = 0
-
-temp = st[0:p]
-for i in temp:
-    cnt[chg(i)] += 1
-
-check = compare(cnt, restrict)
-if sum(check) == 4:
-    res += 1
-
-for i in range(1, s - p + 1):
-    step(st[p - 1 + i], i)
-
-    if sum(check) == 4:
-        res += 1
-
-print(res)
+if tf:
+    for i in res:
+        print(i)
+else:
+    print("NO")
--- study/test2.py
+++ study/test2.py
@@ -1,30 +1,59 @@
 import sys
-sys.stdin = open("input.txt", "r")
+# # input = sys.stdin.readline
+sys.stdin = open("input.txt", 'r')
 
-# import sys
-# input = sys.stdin.readline
+#1211 [S/W 문제해결 기본] 2일차 - Ladder2
+def move(row, col, n):
+    d = [
+        [1, 0], # 아래
+        [0, -1], # 왼쪽
+        [0, 1] # 오른쪽
+    ]
 
-# 10158 개미, 원큐에 도착해야 한다.
-# 가로와 세로를 나누어서 생각한다.
-# 자신의 축의 두 배에 해당하는 값이 넣어지면 제자리로 돌아온다. (좌우로 왔다갔다 했다는 것이다.)
+    go = n
 
-def move(x, y, z): # 현재 위치, 이동, 축의 범위
-    if x + y <= z:
-        return x + y
+    if n == 0:
+        for i in [1, 2]:
+            drow, dcol = row + d[i][0], col + d[i][1]
+
+            if 0 <= drow < 100 and 0 <= dcol < 100:
+                if lst[drow][dcol] == 1:
+                    go = i
+                    break
+
     else:
-        temp = 2*z - x - y
-        if  temp > 0:
-            return temp
-        else:
-            return - temp
+        i = 0
+        drow, dcol = row + d[i][0], col + d[i][1]
+        if 0 <= drow < 100 and 0 <= dcol < 100:
+            if lst[drow][dcol] == 1:
+                go = i
 
-w, h = map(int, input().split()) # 가로 세로
-p, q = map(int, input().split()) # 축의 값
-t = int(input())
-t1 = t % (2 * w)
-t2 = t % (2 * h)
-    
-res1 = move(p, t1, w)
-res2 = move(q, t2, h)
 
-print(res1, res2)
+    drow, dcol = row + d[go][0], col + d[go][1]
+
+    return drow, dcol, go
+
+T = 10
+for test_case in range(1, T + 1):
+    _ = int(input())
+    lst = [list(map(int, input().split())) for i in range(100)]
+    mi = 21e7
+    idx = 0
+
+    for i in range(100):
+        temp_cnt = 1
+        drow = 0
+        dcol = i
+        if lst[drow][dcol] != 1:
+            continue
+
+        n = 0
+        while drow != 99:
+            drow, dcol, n = move(drow, dcol, n)
+            temp_cnt += 1
+
+        if mi > temp_cnt:
+            mi = temp_cnt
+            idx = i
+
+    print(f"#{test_case} {idx} {mi}")
