1a1d8df50d22130595f8baf48f5777a60d367527
Juseong <hdcom37@gmail.com>
2023-02-11 22:41:24 +0900

    ver1.067

--- study/test.py
+++ study/test.py
@@ -4,33 +4,50 @@ sys.stdin = open("input.txt", "r")
 # import sys
 # input = sys.stdin.readline
 
-def my_quick(lst):
-    def sort(start, end):
-        if start >= end:
-            return
+def merge_sort(s, e):
+    if e - s < 1:
+        return
+
+    m = int((s + e)/ 2)
+    merge_sort(s, m)
+    merge_sort(m + 1, e)
+
+    for i in range(s, e + 1):
+        tmp[i] = lst[i]
+    
+    k = s
+    idx1 = s
+    idx2 = m + 1
+    
+    while idx1 <= m and idx2 <= e:
+        if tmp[idx1] > tmp[idx2]:
+            lst[k] = tmp[idx2]
+            k += 1
+            idx2 += 1
+        else:
+            lst[k] = tmp[idx1]
+            k += 1
+            idx1 += 1
         
-        mid = partition(start, end)
-        sort(start, mid - 1)
-        sort(mid, end)
+    while idx1 <= m:
+        lst[k] = tmp[idx1]
+        k += 1
+        idx1 += 1
 
-    def partition(start, end):
-        pivot = lst[(start + end) // 2]
-        while start <= end:
-            while lst[start] < pivot:
-                start += 1
+    while idx2 <= e:
+        lst[k] = tmp[idx2]
+        k += 1
+        idx2 += 1
 
-            while lst[end] > pivot:
-                end -= 1
-        
-            if start <= end:
-                lst[start], lst[end] = lst[end], lst[start]
-                start += 1
-                end -= 1
+##############################################
+n = int(input())
+lst = [0]*(n + 1)
+tmp = [0]*(n + 1)
 
-        return start
+for i in range(1, n + 1):
+    lst[i] = int(input())
 
-    return sort(0, len(lst) - 1)
+merge_sort(1, n)
 
-lst = [3, 1, 2, 4, 5]
-my_quick(lst)
-print(lst)
+for i in range(1, n + 1):
+    print(lst[i])
