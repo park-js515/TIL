a9d9ef43db0a6076c5c03b5daa4706a44c914e07
Juseong <hdcom37@gmail.com>
2023-03-21 23:43:00 +0900

    ver1.140

--- study/test2.py
+++ study/test2.py
@@ -5,58 +5,40 @@ sys.stdin = open("input.txt", 'r')
 # 12851
 
 from collections import deque
+def bfs(num):
+    global k, visited, res
 
-def bfs1(num):
-    global field, visited, k
-
-    visited[num] = 1
+    visited[num] = 0
     q = deque()
     q.append(num)
-    ret = 0
+    cnt = 0
 
     while q:
-        for _ in range(len(q)):
-            now = q.popleft()
+        now = q.popleft()
 
-            if now == k:
-                return ret
-
-            for i in (now - 1, now + 1, now * 2):
-                if 0 <= i < 200000 and not visited[i]:
-                    visited[i] = 1
-                    q.append(i)
-
-        ret += 1
-
-def bfs2(num):
-    global field, MIN, cnt
-
-    q = deque()
-    q.append((num, 0))
-
-    while q:
-        now, time = q.popleft()
-
-        if time > MIN:
+        if visited[now] >= visited[k]:
             continue
 
-        if now == k:
-            cnt += 1
-            continue
+        for i in [now - 1, now + 1, now * 2]:
+            if 0 <= i <= 101000:
+                if visited[i] >= visited[now] + 1:
+                    visited[i] = visited[now] + 1
 
-        time += 1
-        for i in (now - 1, now + 1, now * 2):
-            if 0 <= i <= 200000:
-                q.append((i, time))
+                    if i != k:
+                        q.append(i)
+                    else:
+                        res += 1
 
 
 n, k = map(int, input().split())
 
-field = [0] * 200001
-visited = [0] * 200001
-MIN = bfs1(n)
-cnt = 0
-bfs2(n)
+if n >= k:
+    print(n - k)
+    print(1)
+else:
+    visited = [21e8] * 101001
+    res = 0
+    bfs(n)
 
-print(MIN)
-print(cnt)
+    print(visited[k])
+    print(res)
