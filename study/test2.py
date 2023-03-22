128017532c6d08fdd6e23dc7ebee75efa008b392
박주성 <hdcom37@gmail.com>
2023-03-22 14:10:39 +0900

    ver1.141

--- study/test2.py
+++ study/test2.py
@@ -5,40 +5,47 @@ sys.stdin = open("input.txt", 'r')
 # 12851
 
 from collections import deque
-def bfs(num):
-    global k, visited, res
 
-    visited[num] = 0
+def bfs(num):
+    global visited, v, n, k
+
+    visited[num] = 1
     q = deque()
     q.append(num)
-    cnt = 0
 
     while q:
         now = q.popleft()
 
-        if visited[now] >= visited[k]:
-            continue
-
-        for i in [now - 1, now + 1, now * 2]:
-            if 0 <= i <= 101000:
-                if visited[i] >= visited[now] + 1:
+        for i in (now - 1, now + 1, now * 2):
+            if 0 <= i <= 100000:
+                if not visited[i]:
                     visited[i] = visited[now] + 1
+                    q.append(i)
+                    v[i] = now
 
-                    if i != k:
-                        q.append(i)
-                    else:
-                        res += 1
+                    if i == k:
+                        time = visited[k] - 1
+                        lst = []
+
+                        while i != n:
+                            lst.append(i)
+                            i = v[i]
+
+                        lst.append(n)
+                        lst.reverse()
+
+                        return time, lst
 
 
 n, k = map(int, input().split())
 
 if n >= k:
-    print(n - k)
-    print(1)
+    time = n - k
+    lst = list(range(n, k - 1, -1))
 else:
-    visited = [21e8] * 101001
-    res = 0
-    bfs(n)
+    visited = [0] * 100001
+    v = [0] * 100001
+    time, lst = bfs(n)
 
-    print(visited[k])
-    print(res)
+print(time)
+print(*lst)
